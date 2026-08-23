e9b07cdbe435b741bc67cff7731189bbcfac0574
Dev-kachhadiya <dev.kachhadiya.cg@gmail.com>
2026-08-23 16:48:48 +0530

    variables.py added

---
+++ 02_variables/variables.py
@@ -0,0 +1,42 @@
+#24
+name="Dev"
+age=17
+city="gondal"
+
+print(name,age,city)
+
+#25
+student_name="Darshit"
+roll_no="17"
+student_branch="cse"
+
+print(student_name,roll_no,student_branch)
+
+#26 Reassign
+marks="20"
+marks="25"
+
+print(marks)
+
+#27 several variables in single line
+_name,_age,_city="rahul",18,"patna"
+
+print(_name,_age,_city)
+
+#28 asign to three variables
+x=y=z="0"
+
+print(x,y,z)
+
+#29 valid variables 
+student="rahul"
+studentname="rahul"
+branch="B.Tech"
+
+#30 Reassignment
+studentname_="Dev"
+age_=20
+age_=25
+
+print(studentname_)
+print(age_)
