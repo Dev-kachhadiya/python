159580127bb534c1e6785181067c79319c9421fb
Dev-kachhadiya <dev.kachhadiya.cg@gmail.com>
2026-08-25 23:20:52 +0530

    added airithmeticassign

---
+++ 04_airithmetic_operators/airithmeticassign.py
@@ -0,0 +1,234 @@
+#1 task
+a=10
+b=5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+#2 task
+
+a=10
+b=5.5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+#3 task
+
+a=10
+b=20
+c=30
+
+print(a + b + c)
+print((a + b + c) / 3)
+
+#4 task
+
+p=20
+q=50
+
+print(20 * 50)
+
+#4 task
+
+number = 10
+print(number%2)
+if number % 2 == 0:
+    print("The number is even")
+
+number = 7
+print(number%2)
+if number % 2 != 0:
+    print("The number is odd")
+
+#6 task
+
+a=10
+b=5
+
+print(a / b)
+print(a // b)
+
+a=-10
+b=3
+
+print(a / b)
+print(a // b)
+
+#7 task
+
+a=-10
+b=-2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+#8 task
+
+print(5 - 5)
+print(2 - (-5))
+print((-5) - 5)
+print((-1) - (-2))
+
+#9 task
+
+print(10 // 3)
+print(-10 // 2)
+print(8 // -2)
+print(-4 // -2)
+
+#10 task
+
+print(2 % 1)
+print(-10 % 2)
+print(5 % -4)
+print(-15 % -2)
+
+#11 task
+
+print(10 + 5 * 2)
+print(20 - 4 / 2)
+print(10 + 20 / 5 * 2)
+print(2 + 3 * 4 ** 2)
+print(100 - 20 // 5)
+
+#12 task
+
+print(10 + 5 * 2)
+print((10 + 5) * 2)
+
+print(20 - 10 / 2)
+print((20 - 10) / 2)
+
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+
+#13 task
+
+print(True + True)
+print(True + False)
+print(False * True)
+print(True / True)
+print(True // True)
+print(True % True)
+print(True ** False)
+
+
+#14 task
+
+print(True + 5)
+print(False + 5)
+print(True * 10)
+print(False * 10)
+print(True - 5)
+print(False - 5)
+
+#15 task
+
+print("dev" + "krupa")
+
+#16 task
+
+print("dev" * 3)
+
+#17 task
+
+print("dev" + "krupa")
+print("11" - "world")
+print("hello" * "world")
+print("hello" / "world")
+
+#18 task
+
+value=None
+num=10
+
+print(value + num)
+print(value - num)
+print(value * num)
+print(value / num)
+print(value // num)
+print(value % num)
+print(value ** num)
+
+#19 taskf
+
+print(10 / 0)
+
+Value=None
+Num=10
+
+print(Value + Num)
+
+Str1="Hello"
+Str2="World"
+
+print(Str1- Str2)
+
+#20 task
+
+num1=20 
+num2=6
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+print(num1 ** num2)
+
+#21 task
+
+a = 10
+b = -3
+c = 2.5
+
+# Expression 1: Addition
+print("a + b =", a + b)
+
+# Expression 2: Subtraction
+print("a - c =", a - c)
+
+# Expression 3: Multiplication
+print("b * c =", b * c)
+
+# Expression 4: Division
+print("a / b =", a / b)
+
+# Expression 5: Floor Division
+print("a // b =", a // b)
+
+# Expression 6: Modulus
+print("a % b =", a % b)
+
+# Expression 7: Exponentiation
+print("c * 2 =", c * 2)
+
+# Expression 8: Multiple operators (precedence matters)
+print("a + b * c =", a + b * c)
+
+# Expression 9: Multiple operators with parentheses
+print("(a + b) * c =", (a + b) * c)
+
+# Expression 10: Multiple operators, negative numbers, parentheses
+print("a - (b * 2) / c =", a - (b * 2) / c)
+
+
+
+
